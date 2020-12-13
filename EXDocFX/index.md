0c66083c9e00bbcd50d3edb763013756c73094bd
pig25 <relife25jw@gmail.com>
2020-12-13 14:15:25 +0800

    20201213 Code

---
+++ EXDocFX/index.md
@@ -0,0 +1,15 @@
+
+# 這裡是首頁
+
+# Markdown 副檔名.md 如何撰寫.md檔案
+網址:[Markdown](http://daringfireball.net/projects/markdown/) 
+## 快速連結:
+1. 將圖片加入到image資料夾並參考
+
+
+
+
+# 使用一些插件或圖片的網站
+## Icon8:
+圖片是使用Icon8網站裡的圖片  
+網址:[Icon8](https://icons8.com)
---
+++ 測試DocFX/EX.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 測試DocFX
+{/// <summary>
+ /// 測試類別
+ /// </summary>
+    public class EX
+    {
+        /// <summary>
+        /// 數值
+        /// </summary>
+        public int number { get; set; }
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public EX()
+        {
+
+        }
+        /// <summary>
+        /// 重寫顯示字串
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"數值:{number}";
+        }
+    }
+}
