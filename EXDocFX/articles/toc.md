554c1c16cd127e82f0ef70bb1f97830b7281dc57
pig25 <relife25jw@gmail.com>
2020-12-13 22:54:11 +0800

    20201213 pdf 還是不行

--- EXDocFX/articles/toc.md
+++ EXDocFX/articles/toc.md
@@ -1,1 +1,6 @@
-#[簡介](intro.md)
+#[簡介](intro.md)   
+#[簡介2](intro2.md)  
+#[簡介3](intro3.md)  
+#[簡介4](intro4.md)  
+#[簡介5](intro5.md)  
+
---
+++ 測試DocFX/TEST777/test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 測試DocFX.TEST777
+{
+
+    /// <summary>
+    /// Hello this is **test** from *測試DocFX*
+    /// </summary>
+    public class test
+    {
+        private InnerClass _class;
+        public int Value { get; }
+
+        /// <summary>
+        /// This is a ctor
+        /// </summary>
+        /// <param name="value">The value of the class</param>
+        public test(int value)
+        {
+            Value = value;
+        }
+
+        public double ConvertToDouble()
+        {
+            return Value;
+        }
+
+        /// <summary>
+        /// A method referencing a inner class
+        /// </summary>
+        /// <param name="name">The name</param>
+        /// <param name="inner">A inner class with type <seealso cref="InnerClass"/></param>
+        public void SetInnerClass(string name, InnerClass inner)
+        {
+            inner.Name = name;
+            _class = inner;
+        }
+
+        public class InnerClass
+        {
+            public string Name { get; set; }
+        }
+    }
+}
